52d60569bce3ad1a199735765897a11b6cf20ff5
SirusCodes <darshandrander@gmail.com>
2022-05-09 01:38:14 +0530

    fix: appwrite update prod count function

--- appwrite-functions/update-product-count.py
+++ appwrite-functions/update-product-count.py
@@ -5,21 +5,18 @@ from appwrite.client import Client
 from appwrite.services.database import Database
 
 
-def init_client():
-    """Initialize the Appwrite client"""
+def main(req, resp):
+    env = req.env
+    EVENT_DATA = json.loads(env["APPWRITE_FUNCTION_EVENT_DATA"])
+    EVENT = env["APPWRITE_FUNCTION_EVENT"]
+    product_collection_id = env["APPWRITE_FUNCTION_PRODUCT_COLLECTION_ID"]
+    category_collection_id = env["APPWRITE_FUNCTION_CATEGORY_COLLECTION_ID"]
+
     client = Client()
-    client.set_endpoint(os.getenv("APPWRITE_ENDPOINT"))
-    client.set_project(os.getenv("APPWRITE_FUNCTION_PROJECT_ID"))
-    client.set_key(os.getenv("APPWRITE_API_KEY"))
+    client.set_endpoint(env["APPWRITE_ENDPOINT"])
+    client.set_project(env["APPWRITE_FUNCTION_PROJECT_ID"])
+    client.set_key(env["APPWRITE_API_KEY"])
 
-    return client
-
-def main():
-    EVENT_DATA = json.loads(os.getenv("APPWRITE_FUNCTION_EVENT_DATA"))
-    EVENT = os.getenv("APPWRITE_FUNCTION_EVENT")
-    product_collection_id = os.getenv("APPWRITE_FUNCTION_PRODUCT_COLLECTION_ID")
-
-    client = init_client()
     database = Database(client)
 
     collection_id = EVENT_DATA["$collection"]
@@ -29,14 +26,17 @@ def main():
 
     category_id = EVENT_DATA["categoryId"]
 
-    category = database.get_document(collection_id, category_id)
-    print(category)
+    category = database.get_document(category_collection_id, category_id)
+
     if EVENT == "database.documents.create":
-        category["data"]["productCount"] += 1
+        category["productCount"] += 1
     elif EVENT == "database.documents.delete":
-        category["data"]["productCount"] -= 1
+        category["productCount"] -= 1
 
-    print(category)
+    database.update_document(category_collection_id, category_id, category)
 
-    database.update_document(collection_id, category_id, category["data"])
+    return resp.json({
+        "message": "Successfully updated category count",
+        "category": category["name"]
+    })
 

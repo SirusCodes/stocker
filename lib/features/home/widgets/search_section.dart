a9e911795c9840d8bccbe81a940f6323f8353e41
SirusCodes <darshandrander@gmail.com>
2022-05-09 17:22:49 +0530

    integrate: overall product search

--- lib/features/home/widgets/search_section.dart
+++ lib/features/home/widgets/search_section.dart
@@ -1,17 +1,43 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../shared/widgets/product_list_tile.dart';
+import '../../product/providers/product_overall_search_provider.dart';
 
-class SearchSection extends StatelessWidget {
+class SearchSection extends ConsumerStatefulWidget {
   const SearchSection({Key? key}) : super(key: key);
 
   @override
+  ConsumerState<SearchSection> createState() => _SearchSectionState();
+}
+
+class _SearchSectionState extends ConsumerState<SearchSection> {
+  Timer? _debounce;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _debounce?.cancel();
+  }
+
+  void _onSearchChanged(String query) {
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      ref.read(productOverallSearchProvider.notifier).getResult(query);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final results = ref.watch(productOverallSearchProvider);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).canvasColor,
-        title: const TextField(
-          decoration: InputDecoration(
+        title: TextField(
+          onChanged: _onSearchChanged,
+          decoration: const InputDecoration(
             border: InputBorder.none,
             hintText: "Search products",
             prefixIcon: Icon(Icons.search_rounded),
@@ -19,9 +45,9 @@ class SearchSection extends StatelessWidget {
         ),
       ),
       body: ListView.builder(
-        itemCount: mockProducts.length,
+        itemCount: results.length,
         itemBuilder: (context, index) {
-          final product = mockProducts[index];
+          final product = results[index];
           return ProductListTile(product: product);
         },
       ),

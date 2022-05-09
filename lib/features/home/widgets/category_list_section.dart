9cf0c496ee5527de73cb9440aef3f8ef4835902d
SirusCodes <darshandrander@gmail.com>
2022-05-09 17:02:20 +0530

    integrate: search in category

--- lib/features/home/widgets/category_list_section.dart
+++ lib/features/home/widgets/category_list_section.dart
@@ -2,17 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../shared/widgets/category_list_tile.dart';
+import '../../../shared/widgets/custom_search.dart';
 import '../../../shared/widgets/sort_button.dart';
-import '../../category/domain/domain.dart';
 import '../../category/presentation/save_category_screen.dart';
 import '../../category/providers/category_provider.dart';
+import '../../category/providers/category_search_provider.dart';
 import '../../product/presentation/product_base_screen.dart';
-
-const _categoryList = [
-  CategoryModel(name: "Name zero", color: Colors.amber, productCount: 10),
-  CategoryModel(name: "Name one", color: Colors.red, productCount: 5),
-  CategoryModel(name: "Name two", color: Colors.blue, productCount: 55),
-];
 
 class CategoryListSection extends ConsumerStatefulWidget {
   const CategoryListSection({Key? key}) : super(key: key);
@@ -67,7 +62,7 @@ class _CategoryListSectionState extends ConsumerState<CategoryListSection> {
                   ref.read(categorySortProvider.notifier).state = sort,
             ),
             IconButton(
-              onPressed: () => showSearch(
+              onPressed: () => showCustomSearch(
                 context: context,
                 delegate: _CategorySearchDelegate(),
               ),
@@ -105,7 +100,7 @@ class _CategoryListSectionState extends ConsumerState<CategoryListSection> {
   }
 }
 
-class _CategorySearchDelegate extends SearchDelegate {
+class _CategorySearchDelegate extends CustomSearchDelegate {
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
@@ -126,19 +121,18 @@ class _CategorySearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    if (query.isEmpty) return const SizedBox.shrink();
+    if (query.isEmpty || ref == null) return const SizedBox.shrink();
 
-    final result = _categoryList //
-        .where((cat) => cat.name.toLowerCase().contains(query.toLowerCase()))
-        .toList();
+    final _categories = ref!.watch(categorySearchProvider);
+
     return ListView.builder(
-      itemCount: result.length,
+      itemCount: _categories.length,
       itemBuilder: (context, index) => CategoryListTile(
-        category: result[index],
+        category: _categories[index],
         onTap: () => Navigator.pushNamed(
           context,
           ProductBaseScreen.path,
-          arguments: result[index],
+          arguments: _categories[index],
         ),
       ),
     );
@@ -146,19 +140,25 @@ class _CategorySearchDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    final result = _categoryList //
-        .where((cat) => cat.name.toLowerCase().contains(query.toLowerCase()))
-        .toList();
+    if (ref == null) return const SizedBox.shrink();
+
+    final _categories = ref!.watch(categorySearchProvider);
+
     return ListView.builder(
-      itemCount: result.length,
+      itemCount: _categories.length,
       itemBuilder: (context, index) => CategoryListTile(
-        category: result[index],
+        category: _categories[index],
         onTap: () => Navigator.pushNamed(
           context,
           ProductBaseScreen.path,
-          arguments: result[index],
+          arguments: _categories[index],
         ),
       ),
     );
+  }
+
+  @override
+  void search(String query) {
+    ref?.read(categorySearchProvider.notifier).getResult(query);
   }
 }

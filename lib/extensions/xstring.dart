4d5f25f93802712835418b55e03636eb615ba8ac
SirusCodes <darshandrander@gmail.com>
2022-05-13 01:11:49 +0530

    fix: trim avatar text

--- lib/extensions/xstring.dart
+++ lib/extensions/xstring.dart
@@ -1,9 +1,6 @@
 extension XString on String {
   String get avatarString {
-    final chars = split(" ") //
-        .map((part) => part[0])
-        .join()
-        .toUpperCase();
+    final chars = trim().split(" ").map((part) => part[0]).join().toUpperCase();
     return chars.length > 3 ? chars.substring(0, 3) : chars;
   }
 }
